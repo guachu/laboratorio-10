c010d34ef2e922d6a2a2225f6e37fbd7ef65c61f
juanmora98 <juan.mora-q@mail.escuelaing.edu.co>
2019-04-24 15:56:55 -0500

    commit 2

--- src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
@@ -34,7 +34,7 @@ public class CollabPaintWebSocketConfig extends AbstractWebSocketMessageBrokerCo
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableStompBrokerRelay("/topic/").setRelayHost("192.168.56.101").setRelayPort(61613);
+        config.enableStompBrokerRelay("/topic/").setRelayHost("192.168.56.102").setRelayPort(61613);
         config.setApplicationDestinationPrefixes("/app");        
     }
 
